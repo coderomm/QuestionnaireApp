d97ec55763b79ea2640870161b7bddc7c5fe658b
coder om <mail.coderom@gmail.com>
2024-01-16 14:07:16 +0530

    Initial App Setup

---
+++ src/component/RecommendedProducts.js
@@ -0,0 +1,33 @@
+// RecommendedProducts.js
+import longevit from '../assets/longevit.jpg'
+const recommendedProducts = [
+  {
+    id: 1,
+    name: "LongeVit NMN Supplement Blend | Slow Down Ageing Process (30 Veg Tablets)",
+    image: longevit,
+    description:
+      "Unlock the secret to graceful aging with LongeVit, designed to target multiple hallmarks of aging. Our unique blend, enriched with β NMN, CoQ10, and other potent ingredients, promotes cellular vitality, bolsters immunity, and aids cognitive function. Empower your journey towards slowed aging and enhanced wellness with LongeVit. Always consult a healthcare provider before starting any supplement.",
+    link: "https://decodeage.com/products/longevit-nmn-supplement-blend-slow-down-ageing-process-30-tablets-1",
+  },
+  {
+    id: 2,
+    name: "99% Pure Best Fisetin Supplement, Senolytic Activator 100 mg (30 Veg Capsules)",
+    image: longevit,
+    description: "Fisetin Supplement has recently gained attention in the field of longevity sciences for its potential to target and eliminate senescent or 'zombie' cells. These cells, which accumulate with age, are thought to contribute to age-related diseases and the decline in overall health. Fisetin Supplement is considered one of the most powerful agents for alleviating senescent cell-related damage.",
+    link: "https://decodeage.com/products/fisetin",
+  },{
+    id: 3,
+    name: "SportVit NMN Supplement Blend for Performing Athletes (30 Veg Capsules)",
+    image: longevit,
+    description: "SportVit is a Powerful Blend of NMN with L-Taurin and Inositol, and watch your athletic performance and aerobic endurance shoot up! Usually, when we want sustained energy levels throughout the day, we turn to options like Caffeine. But, did you know that the best way to harness your peak performance is to supplementing with NMN Supplement and utilising the naturally occurring energy within your cells?",
+    link: "https://decodeage.com/products/sportvit",
+  },{
+    id: 4,
+    name: "Decode Biome- The Most Advanced Gut Microbiome Test for Gut Health",
+    image: longevit,
+    description: "Discover your gut health's full potential with Decode Biome, India's leading gut microbiome test kit. Our test employs advanced shotgun sequencing technology to analyze your gut ecosystem's bacteria, viruses, fungi, and archaea. We offer personalized insights to enhance digestion, boost immunity, manage weight, and improve your overall wellness. Start your tailored journey to optimal gut health today with Decode Biome, your ultimate guide to gut health.",
+    link: "https://decodeage.com/products/decode-biome-the-most-advanced-gut-microbiome-test-for-gut-health",
+  },
+];
+
+export default recommendedProducts;
